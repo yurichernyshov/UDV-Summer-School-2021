34153b02f71ff1c1b3f829de20aebedbd664f14c
SpaciSoxrani <thorloky007@gmail.com>
2021-07-27 20:33:09 +0500

    added two projects for AutoML (classification and regression algorithms)

---
+++ tasks/Smirnova_Anastasiya_AutoML(regression_and_classification)/AutoML_BinaryClassification/AutoMLApp/DataStructures/SentimentPrediction.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.ML.Data;
+
+namespace AutoMLApp.DataStructures
+{
+    class SentimentPrediction
+    {
+        // ColumnName attribute is used to change the column name from
+        // its default value, which is the name of the field.
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Score { get; set; }
+    }
+}
