7906596023fb1df9cbc593c47748a3eb97486652
KirillKalutsky <kefirchik.2000@mail.ru>
2021-07-28 21:23:28 +0500

    модель анализа текстов

---
+++ tasks/KirillKalutsky_SentimentAssessmentAnalysis/IMDBCommentClassification/Models/SentimentData.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBCommentClassification.Models
+{
+    [Serializable()]
+    class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText;
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment;
+    }
+}
---
+++ tasks/KirillKalutsky_SentimentAssessmentAnalysis/IMDBCommentClassification/Models/SentimentPrediction.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.ML.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IMDBCommentClassification.Models
+{
+    class SentimentPrediction: SentimentData
+    {
+        [ColumnName("PredictedLabel")]
+        public bool Prediction { get; set; }
+
+        public float Probability { get; set; }
+
+        public float Score { get; set; }
+    }
+}
