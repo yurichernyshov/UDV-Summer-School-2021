486ec12e1d36f510d3494a33c7f26c554384015f
KirillKalutsky <kefirchik.2000@mail.ru>
2021-07-28 22:12:57 +0500

    реализовать модель машинного обучения в production

---
+++ tasks/KirillKalutsky_WEB_API/CoreApi/Controllers/ViewController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using CoreApi.ML.Models;
+using Microsoft.Extensions.ML;
+
+namespace CoreApi.Controllers
+{
+    public class ViewController : Controller
+    {
+
+        private readonly PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool;
+
+        public ViewController(PredictionEnginePool<SentimentData, SentimentPrediction> predictionEnginePool)
+        {
+            this.predictionEnginePool = predictionEnginePool;
+        }
+
+        [HttpGet("")]
+        public ActionResult Index()
+        {
+            //ViewBag.Message = null;
+            return View("Views/PredictPage.cshtml");
+        }
+        
+        [HttpPost("Predict")]
+        public ActionResult UpdateIndex([FromForm] SentimentData comment)
+        {
+            SentimentPrediction prediction = predictionEnginePool.Predict(modelName: "SentimentAnalysisModel", example: comment);
+            comment.Sentiment = prediction.Prediction;
+            ViewBag.Message = prediction;
+            return Index();
+        }
+    }
+}
---
+++ tasks/KirillKalutsky_WEB_API/CoreApi/ML/Models/SentimentData.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.ML.Data;
+using System;
+
+
+namespace CoreApi.ML.Models
+{
+    [Serializable()]
+    public class SentimentData
+    {
+        [LoadColumn(0)]
+        public string SentimentText { get; set; }
+
+        [LoadColumn(1), ColumnName("Label")]
+        public bool Sentiment { get; set; }
+
+    }
+}
